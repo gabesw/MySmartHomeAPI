0da2bef573b5366680fc643c567e5e15fb4f4da1
Gabriel Woloz <wolozgabriel@gmail.com>
2023-12-10 14:57:08 -0500

    converted to viewset views and implemented kitchen light switch

--- MySmartHomeAPI/API/serializers.py
+++ MySmartHomeAPI/API/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from API.models import KitchenKeepOnSwitch
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,16 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+class KitchenLightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KitchenKeepOnSwitch
+        fields = ['val']
+
+    def validate_val(self, value):
+        """
+        Check that the val is between 0 and 2.
+        """
+        if not (0 <= value <= 2):
+            raise serializers.ValidationError("Value must be between 0 and 2.")
+        return value
