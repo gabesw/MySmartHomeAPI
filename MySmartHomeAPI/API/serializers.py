015fca8cc745e75a713495cc284f35246439db7a
Gabriel Woloz <wolozgabriel@gmail.com>
2023-12-10 17:44:37 -0500

    added token authentication

--- MySmartHomeAPI/API/serializers.py
+++ MySmartHomeAPI/API/serializers.py
@@ -6,7 +6,16 @@ from API.models import KitchenKeepOnSwitch
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['url', 'username', 'email', 'password', 'groups']
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
